f058b93e3eb934a78069908fbb7d156588d03c75
bajajku <bajajku@sheridancollege.ca>
2025-03-30 05:34:23 -0400

    chore: update backend URL to point to production environment

--- apps/frontend/src/utils/index.ts
+++ apps/frontend/src/utils/index.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = "https://decentralized-app-6b2a.onrender.com";
 export const CLOUDFRONT_URL = "https://d17906afh70r4o.cloudfront.net"
